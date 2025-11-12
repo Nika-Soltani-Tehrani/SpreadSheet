fe1e51d06282ed108974c9f277ca5b188f6a95e6
Nika-Soltani-Tehrani <nsoltanitehraniw@gmail.com>
2025-11-12 22:46:07 +0100

    added load and store functionalities

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,29 @@ import java.util.Map;
 
 public class Main {
 
-    public void printSpreadsheetWithValues(Spreadsheet spreadsheet) {
+    public static void printSpreadsheetWithValues(Spreadsheet spreadsheet) {
         for (Map.Entry<Coordinate, Cell> entry : spreadsheet.getSpreadsheet().entrySet()) {
             Coordinate coord = entry.getKey();
             Cell cell = entry.getValue();
-            try {
-                Double value = cell.getValue(spreadsheet);
-                System.out.println(coord + " = " + value);
-            } catch (Exception e) {
-                System.out.println(coord + " = ERROR (" + e.getMessage() + ")");
-            }
+            System.out.println(coord + " = " + cell.getContent().asString());
         }
     }
 
 
     public static void main(String[] args) {
-//        Spreadsheet sheet = new Spreadsheet();
+        Spreadsheet sheet = new Spreadsheet();
+        sheet.loadSpreadsheetFromFile("spreadsheet.s2v");
+        printSpreadsheetWithValues(sheet);
 //
-//        sheet.setCellContent("A1", "=C1+C2");
-//        sheet.setCellContent("B1", "4");
+        sheet.setCellContent("A1", "=C1+B2");
+        sheet.setCellContent("B1", "3");
 //        sheet.setCellContent("C1", "1");
 //        sheet.setCellContent("C2", "2");
 //        sheet.setCellContent("B3", "TOTAL");
 //        sheet.setCellContent("C3", "=A1+B1");
 //
-//        sheet.storeSpreadsheetInFile("spreadsheet.s2v");
-
-        Spreadsheet sheet = new Spreadsheet();
-        sheet.loadSpreadsheetFromFile("spreadsheet.s2v");
-
-        for (Map.Entry<Coordinate, Cell> entry : sheet.getSpreadsheet().entrySet()) {
-            System.out.println(entry);
-            Coordinate coord = entry.getKey();
-            Cell cell = entry.getValue();
-            System.out.println(coord + " = " + cell.getContent().asString());
-        }
+        sheet.storeSpreadsheetInFile("spreadsheet.s2v");
+        System.out.println("Spreadsheet after modification");
+        printSpreadsheetWithValues(sheet);
     }
 }
